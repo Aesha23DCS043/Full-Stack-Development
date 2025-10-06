bc0967dcbffb78e28bb8ec0c95797d4e7d6e7341
Aesha23DCS043 <23dcs043@charusat.edu.in>
2025-10-06 12:52:10 +0530

    FSD_Project

---
+++ Mini_Project/my-bath-app/src/main.jsx
@@ -0,0 +1,25 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { ToastProvider } from './context/ToastContext';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <ToastProvider>
+          <AuthProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </AuthProvider>
+        </ToastProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
